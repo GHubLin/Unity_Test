452e6f2164520a7eaf77895898f03347e30ac56c
LinZH <102825005+GHubLin@users.noreply.github.com>
2022-05-21 17:39:36 +0800

    Add files via upload

---
+++ coincollider.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class coincollider : MonoBehaviour
+{
+    public Text textcoin;
+    public int textcount;
+
+
+    private void OnTriggerEnter(Collider coin)
+    {
+        if (coin.tag == "coin")
+        {
+            Debug.Log("dadsda");
+            textcount++;
+            textcoin.text = textcount.ToString();
+            Destroy(coin.gameObject);
+
+        }
+        
+
+
+    }
+}
